cc4d3752b1f1e2f722656b89fe837f08411ac295
durraniarts <abdurrehmandurrani1215@gmail.com>
2025-11-13 19:21:12 +0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" flex items-center  min-h-screen">
+      <div className="font-over mx-auto  select-none text-black flex flex-col items-end ">
+        <h1 className="text-[25vw] leading-tight">DREAM.</h1>
+        <h2 className="text-black text-[4vw] font-alumni font-extrabold opacity-80">
+          Durrani-arts is under development ___
+        </h2>
+      </div>
+    </main>
+  );
+}
