0d61acf602fadfe47bee60355ad8a44fed711cba
durraniarts <abdurrehmandurrani1215@gmail.com>
2025-11-13 19:48:03 +0500

    font update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className=" flex items-center  min-h-screen">
       <div className="font-over mx-auto  select-none text-black flex flex-col items-end ">
         <h1 className="text-[25vw] leading-tight">DREAM.</h1>
-        <h2 className="text-black text-[4vw] font-alumni font-extrabold opacity-80">
+        <h2 className="text-black text-[4vw] font-sans font-extrabold opacity-80">
           Durrani-arts is under development ___
         </h2>
       </div>
